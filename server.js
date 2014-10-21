b020cd5960b15eec8a0f3a4644d83eb09cfa95f9
pixelskull <pascal.schoenthier@googlemail.com>
2014-10-21 13:45:03 +0200

    location information is send over Socket

--- server.js
+++ server.js
@@ -16,10 +16,17 @@ io.on('connection', function(socket) {
   console.log('a new connection');
 
   socket.on('sensor', function(data) {
+    // do something with sensordata
+    console.log(data);
+  });
+
+  socket.on('location', function(data) {
+    // do something with locationinformation
     console.log(data);
   });
 
   socket.on('disconnection', function(){
+    // do something with disconnedtioninformation 
     console.log('a disconnection');
   });
 });
